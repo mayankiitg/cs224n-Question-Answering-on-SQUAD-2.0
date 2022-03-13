0b012f1b877862ed6d17c050a3a4e489242ecf63
samidhpratap <32315701+samidhpratap@users.noreply.github.com>
2022-03-12 16:45:19 -0800

    add model_dict

--- util.py
+++ util.py
@@ -337,6 +337,7 @@ def load_model(model, checkpoint_path, gpu_ids, return_step=True):
 
     # Build model, load parameters
     #filter out unnecessary keys
+    model_dict = model.state_dict()
     ckpt_dict_cut1 = {k:v for k, v in ckpt_dict['model_state'].items() if k in model_dict}
     ckpt_dict_cut = {k:v for k, v in ckpt_dict_cut1.items() if ckpt_dict_cut1[k].shape==model_dict[k].shape}
     model_dict.update(ckpt_dict_cut)
